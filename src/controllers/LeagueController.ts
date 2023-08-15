fb8798c49ac7c1f04e53f19a7f1f0a41f151f757
keiru517 <adrianbotteon@gmail.com>
2023-08-15 10:04:17 +0300

    complete: league apis

--- src/controllers/LeagueController.ts
+++ src/controllers/LeagueController.ts
@@ -1,16 +1,60 @@
 import { RequestHandler } from 'express';
 import League from '../models/League';
-import { Types } from '../types';
+// import { Types } from '../types';
 
+// GET SERVER_URL/api/league/all
 export const all: RequestHandler = async (req, res) => {
   const leagues = await League.findAll();
   res.json({ leagues });
 };
 
+// POST SERVER_URL/api/league/create
 export const create: RequestHandler = async (req, res) => {
-  const json: Types.T_League = req.body;
-  const league = await League.create(json);
+  // const json: Types.T_League = req.body;
+  const data = {
+    name: req.body.name,
+    description: req.body.description,
+    logo: req.body.logo,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate
+  };
+
+  const league = await League.create(data);
   res.json({ league });
+};
+
+// POST SERVER_URL/api/league/update/1
+export const update: RequestHandler = async (req, res) => {
+  // const id = Number(req.params.id);
+
+  const data = {
+    name: req.body.name,
+    description: req.body.description,
+    logo: req.body.logo,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate
+  };
+
+  const league = await League.findByPk(req.body.id);
+  if (league) {
+    await league.update(data);
+    res.json({ league });
+  } else {
+    res.status(404).json({ message: 'league not found' });
+  }
+};
+
+// POST SERVER_URL/api/league/remove/1
+export const remove: RequestHandler = async (req, res) => {
+  const id = Number(req.params.id);
+
+  const league = await League.findByPk(id);
+  if (league) {
+    await league.destroy();
+    res.json({ message: 'deleted successfully!' });
+  } else {
+    res.status(404).json({ message: 'league not found' });
+  }
 };
 
 export const info: RequestHandler = async (req, res) => {
--- src/routers/league.ts
+++ src/routers/league.ts
@@ -2,7 +2,15 @@ import { LeagueController } from '../controllers';
 import { Router } from 'express';
 const leagueRouter = Router();
 
+// get all leagues
 leagueRouter.get('/all', LeagueController.all);
+// create a league
 leagueRouter.post('/create', LeagueController.create);
+// update a league
+leagueRouter.post('/update', LeagueController.update);
+// delete a league
+leagueRouter.delete('/remove/:id', LeagueController.remove);
+// get a league
 leagueRouter.get('/info/:id', LeagueController.info);
+
 export default leagueRouter;
