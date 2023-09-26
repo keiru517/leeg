d7704283055f6ecfab5ac223cce6ffe836671040
keiru517 <adrianbotteon@gmail.com>
2023-09-26 18:04:55 +0300

    updated the player history

--- src/components/Table/Player.jsx
+++ src/components/Table/Player.jsx
@@ -7,8 +7,9 @@ import { useSelector } from "react-redux";
 const Player = (props) => {
   // const { teams } = props;
   const { players } = props;
-  const columns = ["Player", "Jersery Number", "Team"];
-  const teams = useSelector(state=>state.home.teams);
+  const columns = ["Player", "Jersery Number", "Points", "Team"];
+  const teams = useSelector((state) => state.home.teams);
+  const matchups = useSelector((state) => state.home.matchups);
 
   // const players = useSelector((state) => state.home.players).filter(
   //   (player) => player.teamId !== 0 // Filter players who are added to the team
@@ -38,47 +39,70 @@ const Player = (props) => {
         <tbody className="text-center">
           {
             // teams.map(({ logo, name}, index) =>
-            players.filter(player=>player.teamId !== 0).map((player, idx) => (
-              <tr
-                key={idx}
-                className="odd:bg-light-dark-gray dark:odd:bg-dark-gray even:bg-light-charcoal dark:even:bg-charcoal h-[53px]"
-              >
-                <td className="w-1/4">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal flex items-center underline justify-between px-10"
-                  >
-                    <img src={player.avatar} alt="" className="h-8 w-8 mr-2 rounded-default" />
-                    <Link to={`player/${player.userId}`}>{player.firstName} {player.lastName}</Link>
-                  </Typography>
-                </td>
-                <td className="w-1/4">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {player?.jerseyNumber}
-                  </Typography>
-                </td>
-                <td className="w-1/5">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal flex items-center underline justify-between px-10"
-                  >
-                    <img
-                      src={teams.find((team) => team.id == player.teamId)?.logo}
-                      alt=""
-                      className="w-8 h-8 mr-2 rounded-default"
-                    />
-                    <Link to={`team/${player.teamId}`}>
-                      {teams.find((team) => team.id == player.teamId)?.name}
-                    </Link>
-                  </Typography>
-                </td>
-                {/* <td className="w-1/5"> 
+            players
+              .filter((player) => player.teamId !== 0)
+              .map((player, idx) => (
+                <tr
+                  key={idx}
+                  className="odd:bg-light-dark-gray dark:odd:bg-dark-gray even:bg-light-charcoal dark:even:bg-charcoal h-[53px]"
+                >
+                  <td className="w-1/4">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal flex items-center underline justify-between px-10"
+                    >
+                      <img
+                        src={player.avatar}
+                        alt=""
+                        className="h-8 w-8 mr-2 rounded-default"
+                      />
+                      <Link to={`player/${player.userId}`}>
+                        {player.firstName} {player.lastName}
+                      </Link>
+                    </Typography>
+                  </td>
+                  <td className="w-1/4">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {player?.jerseyNumber}
+                    </Typography>
+                  </td>
+                  <td className="w-1/4">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {
+                        matchups.filter(
+                          (matchup) => matchup.playerId == player?.id
+                        ).reduce((accumulator, currentMatchup) => accumulator + currentMatchup.points, 0)
+                      }
+                    </Typography>
+                  </td>
+                  <td className="w-1/5">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal flex items-center underline justify-between px-10"
+                    >
+                      <img
+                        src={
+                          teams.find((team) => team.id == player.teamId)?.logo
+                        }
+                        alt=""
+                        className="w-8 h-8 mr-2 rounded-default"
+                      />
+                      <Link to={`team/${player.teamId}`}>
+                        {teams.find((team) => team.id == player.teamId)?.name}
+                      </Link>
+                    </Typography>
+                  </td>
+                  {/* <td className="w-1/5"> 
                   <Typography
                     variant="small"
                     color="blue-gray"
@@ -96,8 +120,8 @@ const Player = (props) => {
                     {player.jerseyNumber}
                   </Typography>
                 </td> */}
-              </tr>
-            ))
+                </tr>
+              ))
             // )
           }
         </tbody>
