1f2fd806e32cbe48e2c3d1e24b0dc7d7e9da3090
keiru517 <adrianbotteon@gmail.com>
2023-10-20 01:42:27 +0300

    removed logo

--- src/pages/home/league.jsx
+++ src/pages/home/league.jsx
@@ -692,8 +692,8 @@ const League = () => {
                   )}
                 >
                   <hr className="h-px my-4 bg-charcoal border-0" />
-                  <div className="flex h-full space-x-4">
-                    <div className="w-1/2 bg-light-charcoal dark:bg-charcoal flex flex-col h-full min-h-[420px] p-default rounded-main">
+                  <div className="h-full grid grid-cols-1 sm:grid-cols-2 sm:space-x-4 sm:space-y-0 space-y-3 ">
+                    <div className="bg-light-charcoal dark:bg-charcoal flex flex-col h-full min-h-[420px] p-default rounded-main">
                       <div className="flex justify-between w-full">
                         <p className="text-black dark:text-white text-xl font-semibold">
                           Waitlisted Players
@@ -761,7 +761,7 @@ const League = () => {
                         )}
                       </div>
                     </div>
-                    <div className="w-1/2 bg-light-charcoal dark:bg-charcoal flex flex-col h-full min-h-[420px] p-default rounded-main">
+                    <div className="bg-light-charcoal dark:bg-charcoal flex flex-col h-full min-h-[420px] p-default rounded-main">
                       <div className="flex justify-between w-full">
                         <p className="text-black dark:text-white text-xl font-semibold">
                           Available Players
--- src/pages/signin/index.jsx
+++ src/pages/signin/index.jsx
@@ -40,14 +40,14 @@ const Signin = () => {
   };
 
   return (
-    <div className="sm:w-auth sm:mx-auto sm:mt-32">
-      <div className="w-[164px] h-[185px] mx-auto">
+    <div className="sm:w-auth sm:mx-auto">
+      {/* <div className="w-[164px] h-[185px] mx-auto">
         <div className="flex w-[112px] h-[112px] bg-white dark:bg-slate rounded-full items-center mx-auto">
           <img src={logo} alt="logo" className="mx-auto w-[38px] h-[38px]" />
         </div>
         <img src={hrLine} alt="" className="my-7" />
         <p className="text-font-light-gray text-sm text-center">LEEG.IO</p>
-      </div>
+      </div> */}
       <div className="bg-white dark:bg-slate w-full h-[390px] mt-16 rounded-main p-default flex flex-col">
         <div>
           <div>
