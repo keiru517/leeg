1dad70a81fd2feadd4d2541af67f3a1f1887a9ce
keiru517 <adrianbotteon@gmail.com>
2024-02-05 04:31:13 +0200

    updated blog

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ import { absolutePath } from '../helpers';
 const options: { [key: string]: Options } = {
   development: {
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || undefined,
+    password: process.env.DB_PASS || "",
     database: process.env.DB_NAME || 'leegDev',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql'
