f00c6fc7161835f3bd9eb6e26a4e016c269480ce
keiru517 <adrianbotteon@gmail.com>
2024-02-14 03:34:13 +0200

    updated dashboard

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -339,7 +339,7 @@ const Dashboard = () => {
         <>
             <hr className="h-px mb-4 bg-charcoal border-0" />
             <div className="flex space-x-4">
-                <div className="w-full flex flex-col p-default rounded-main">
+                <div className="w-full flex flex-col">
                     {/* <div className="flex justify-between w-full">
                         <p className="text-black dark:text-white text-xl font-semibold">
                             Dashboard
